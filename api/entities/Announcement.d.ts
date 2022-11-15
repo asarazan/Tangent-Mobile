3324cf0d39bb53c46c94d24dbd5d9437793587da
Aaron Sarazan <aaron@sarazan.net>
2022-11-14 20:38:22 -0800

    add announcements

---
+++ api/entities/Announcement.d.ts
@@ -0,0 +1,100 @@
+import {AnnouncementReaction} from "./AnnouncementReaction";
+
+/**
+ * Represents an announcement set by an administrator.
+ * @see https://docs.joinmastodon.org/entities/announcement/
+ */
+export type Announcement = {
+
+  // Base Attributes
+
+  /**
+   * Description: The announcement id.
+   * Type: String (cast from an integer, but not guaranteed to be a number)
+   * Version history:
+   * 3.1.0 - added
+   */
+  id: string;
+
+  /**
+   * Description: The content of the announcement.
+   * Type: String
+   * Version history:
+   * 3.1.0 - added
+   */
+  text: string;
+
+  /**
+   * Description: Whether the announcement is currently active.
+   * Type: Boolean
+   * Version history:
+   * 3.1.0 - added
+   */
+  published: boolean;
+
+  /**
+   * Description: Whether the announcement has a start/end time.
+   * Type: Boolean
+   * Version history:
+   * 3.1.0 - added
+   */
+  all_day: boolean;
+
+  /**
+   * Description: When the announcement was created.
+   * Type: String (ISO 8601 Datetime)
+   * Version history:
+   * 3.1.0 - added
+   */
+  created_at: string;
+
+  /**
+   * Description: When the announcement was last updated.
+   * Type: String (ISO 8601 Datetime)
+   * Version history:
+   * 3.1.0 - added
+   */
+  updated_at: string;
+
+  /**
+   * Description: Whether the announcement has been read by the user.
+   * Type: Boolean
+   * Version history:
+   * 3.1.0 - added
+   */
+  read: boolean;
+
+  /**
+   * Description: Emoji reactions attached to the announcement.
+   * Type: Array of AnnouncementReaction
+   * Version history:
+   * 3.1.0 - added
+   */
+  reactions: AnnouncementReaction[];
+
+  // Optional Attributes
+
+  /**
+   * Description: When the future announcement was scheduled.
+   * Type: String (ISO 8601 Datetime)
+   * Version history:
+   * 3.1.0 - added
+   */
+  scheduled_at: string;
+
+  /**
+   * Description: When the future announcement will start.
+   * Type: String (ISO 8601 Datetime)
+   * Version history:
+   * 3.1.0 - added
+   */
+  starts_at: string;
+
+  /**
+   * Description: When the future announcement will end.
+   * Type: String (ISO 8601 Datetime)
+   * Version history:
+   * 3.1.0 - added
+   */
+  ends_at: string;
+};
---
+++ api/entities/AnnouncementReaction.d.ts
@@ -0,0 +1,53 @@
+/**
+ * Represents an emoji reaction to an Announcement.
+ * @see https://docs.joinmastodon.org/entities/announcementreaction/
+ */
+export type AnnouncementReaction = {
+
+  // Base Attributes
+
+  /**
+   * Description: The emoji used for the reaction.
+   * Either a unicode emoji, or a custom emoji's shortcode.
+   * Type: String
+   * Version history:
+   * 3.1.0 - added
+   */
+  name: string;
+
+  /**
+   * Description: The total number of users who have added this reaction.
+   * Type: Number
+   * Version history:
+   * 3.1.0 - added
+   *
+   * @precision int
+   */
+  count: number;
+
+  /**
+   * Description: Whether the authorized user has added this reaction to the announcement.
+   * Type: Boolean
+   * Version history:
+   * 3.1.0 - added
+   */
+  me: boolean;
+
+  // Custom Emoji Attributes
+
+  /**
+   * Description: A link to the custom emoji.
+   * Type: String (URL)
+   * Version history:
+   * 3.1.0 - added
+   */
+  url: string;
+
+  /**
+   * Description: A link to a non-animated version of the custom emoji.
+   * Type: String (URL)
+   * Version history:
+   * 3.1.0 - added
+   */
+  static_url: string;
+};
