c1661c26c4d88e503858577803754ecb45ebb62a
Aaron Sarazan <aaron@sarazan.net>
2022-11-15 09:20:39 -0800

    give scheduled status a try

--- api/entities/ScheduledStatus.d.ts
+++ api/entities/ScheduledStatus.d.ts
@@ -1,1 +1,25 @@
-// TODO!
+/**
+ * Represents a status that will be published at a future scheduled date.
+ * @see https://docs.joinmastodon.org/entities/scheduledstatus/
+ */
+export type ScheduledStatus = {
+
+  // Required Attributes
+
+  /**
+   * Description: ID of the scheduled status in the database.
+   * Type: String (cast from an integer but not guaranteed to be a number)
+   * Version history: Added in 2.7.0
+   */
+  id: string;
+
+  /**
+   * Description: ID of the status in the database.
+   * Type: String (ISO 8601 Datetime)
+   * Version history: Added in 2.7.0
+   */
+  scheduled_at: string;
+
+  // The rest are under construction.
+  // We'll add them once they're finalized!
+};
