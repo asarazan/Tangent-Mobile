962c4efbe037b97b0812b29c1dbc00663fb20096
Aaron Sarazan <aaron@sarazan.net>
2022-11-19 14:11:22 -0800

    latest martok changes

--- build_schema.sh
+++ build_schema.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm -rf shared/src/commonMain/kotlin/social/tangent/mobile/api
-martok api -t -s -a -d standard -p social.tangent.mobile.api -o shared/src/commonMain/kotlin/social/tangent/mobile/api
+martok api -i -t -s -a -d standard -p social.tangent.mobile.api -o shared/src/commonMain/kotlin/social/tangent/mobile/api
